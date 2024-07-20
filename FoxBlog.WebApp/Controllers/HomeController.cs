08a4e8a2c1d6b552b74677f3164deb30fec91204
Jagdender <3027297767@qq.com>
2024-07-20 17:27:22 +0800

    refactor: Optimize the i18n of `Home` page.
    - Make the content dynamic in markdowns, instead of cshtmls.

--- FoxBlog.WebApp/Controllers/HomeController.cs
+++ FoxBlog.WebApp/Controllers/HomeController.cs
@@ -1,21 +1,14 @@
 using System.Diagnostics;
 using FoxBlog.WebApp.Models;
-using FoxBlog.WebApp.Views.Shared.Resources;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Localization;
 
 namespace FoxBlog.WebApp.Controllers
 {
-    public class HomeController(IStringLocalizer<SharedLangResource> lang) : Controller
+    public class HomeController : Controller
     {
-        private readonly IStringLocalizer<SharedLangResource> _lang = lang;
-
         public IActionResult Index()
         {
-            if (_lang["Current"] == "zh")
-                return View("Index.zh");
-            else
-                return View("Index.en");
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ FoxBlog.WebApp/wwwroot/md/home.en.md
@@ -0,0 +1,11 @@
+### Hi👋, this is Jagdender.
+
+~~They also call me 🦊~~
+
+<br/>
+
+I am a software developer using various technologies, mainly **.NET**. I'm currently engaging in the area of **Web**, **Desktop** and **Software Architecture**.
+
+The blog records my learning journey and my thoughts on various technologies.
+
+I'll also post some of my daily life on it, just like what I do on my SNS.
